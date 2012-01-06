8c90c124bcdde27ff9cbf771821063c763edf5d9
Lynn <lynn@lynn-desktop.(none)>
2012-01-06 22:10:38 +0800

    impl of agent, this version is only a struct code, some details will be add later

--- src/agent/AgentI.cpp
+++ src/agent/AgentI.cpp
@@ -1,16 +1,69 @@
 
 #include "src/agent/AgentI.h"
 
-::Ice::Int
-xlog::AgentI::ensureLog(const ::Ice::StringSeq& lines,
-                        const Ice::Current& current)
+namespace xlog {
+
+AgentI::AgentI() 
 {
-    return 0;
+	normalSendWorker_ = new NormalSendWorker;
+	normalSendWorker_->start().detach();
+	failedSendWorker_ = new FailedSendWorker;
+	failedSendWorker_->start().detach();
 }
 
-void
-xlog::AgentI::dobestLog(const ::Ice::StringSeq& lines,
-                        const Ice::Current& current)
+void AgentI::add(const LogDataSeq& datas, const ::Ice::Current& current)
 {
+	normalSendWorker_->add(datas);
+}
+    
+void AgentI::addFailedLogDatas(const LogDataSeq& datas, const ::Ice::Current& current)
+{
+	failedSendWorker_->add(datas);
 }
 
+::Ice::StringSeq AgentI::getAgents(const ::Ice::Current& current)
+{
+	::IceUtil::Mutex::Lock lock(agentsMutex_);
+	return agents_;
+}
+
+void SendWorker::add(const LogDataSeq& datas)
+{
+	::IceUtil::Monitor< ::IceUtil::Mutex>::Lock lock(datasMutex_);
+	datas_.insert(datas_.end(), datas.begin(), datas.end());
+	datasMutex_.notify();
+}
+
+void SendWorker::run()
+{
+	while(true)
+	{
+		LogDataSeq datas;
+		{
+			::IceUtil::Monitor< ::IceUtil::Mutex>::Lock lock(datasMutex_);
+			if(datas_.empty())
+			{
+				datasMutex_.wait();
+			}
+			datas.swap(datas_);
+		}
+		if(!send(datas))
+		{
+			add(datas);
+		}
+	}
+}
+
+bool NormalSendWorker::send(const LogDataSeq& datas)
+{
+	//TODO
+	return true;
+}
+
+bool FailedSendWorker::send(const LogDataSeq& datas)
+{
+	//TODO
+	return true;
+}
+
+}
--- src/agent/AgentI.h
+++ src/agent/AgentI.h
@@ -2,19 +2,92 @@
 #define __AgentI_h__
 
 #include <xlog.h>
+#include <IceUtil/Thread.h>
+#include <IceUtil/Mutex.h>
+#include <IceUtil/Monitor.h>
 
 namespace xlog
 {
+
+class NormalSendWorker;
+typedef ::IceUtil::Handle<NormalSendWorker> NormalSendWorkerPtr;
+
+class FailedSendWorker;
+typedef ::IceUtil::Handle<FailedSendWorker> FailedSendWorkerPtr;
 
 class AgentI : virtual public Agent
 {
 public:
 
-    virtual ::Ice::Int ensureLog(const ::Ice::StringSeq&,
-                                 const Ice::Current&);
+	AgentI();
 
-    virtual void dobestLog(const ::Ice::StringSeq&,
-                           const Ice::Current&);
+public:
+
+	/**
+	 * 收集client的log信息
+	 * @param datas log信息
+	 */
+    virtual void add(const LogDataSeq& datas, const ::Ice::Current& current);
+   
+	/**
+	 * 收集client曾经发送失败的log信息
+	 * @param datas log信息
+	 */
+	virtual void addFailedLogDatas(const LogDataSeq& datas, const ::Ice::Current& current);
+	
+	/**
+	 * 获取所有agent的字符串信息，可以通过该字符串生成agent的prx
+	 */
+	virtual ::Ice::StringSeq getAgents(const ::Ice::Current& current);
+
+private:
+
+	::Ice::StringSeq agents_; /** 当前所有的agent信息*/
+	
+	::IceUtil::Mutex agentsMutex_; /**agents的锁*/
+
+	NormalSendWorkerPtr normalSendWorker_; /**发送正常数据的worker*/
+	
+	FailedSendWorkerPtr failedSendWorker_; /**发送失败数据的worker*/
+	
+};
+
+class SendWorker : public ::IceUtil::Thread 
+{
+public:
+
+	void add(const LogDataSeq& datas);
+
+protected:
+
+	virtual void run();
+
+	/**
+	 * 发送数据的接口
+	 * 不同的worker会调用dispatcher的不同接口
+	 */
+	virtual bool send(const LogDataSeq& datas) = 0;
+
+private:
+
+	LogDataSeq datas_;
+	
+	::IceUtil::Monitor< ::IceUtil::Mutex> datasMutex_;
+};
+
+class NormalSendWorker : public SendWorker
+{
+protected:
+
+	virtual bool send(const LogDataSeq& datas);
+
+};
+
+class FailedSendWorker : public SendWorker
+{
+protected:
+	virtual bool send(const LogDataSeq& datas);
+
 };
 
 }
